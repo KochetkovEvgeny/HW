56b495b1358c4fe16cacaab2636d24197b5b7036
Евгений Кочетков <evgenijkocetkov@MacBook-Air-Evgenij.local>
2025-12-04 17:33:41 +0300

    тесты

--- test_api.py
+++ test_api.py
@@ -2,9 +2,6 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-
-
-
 def test_child():
     testclient = TestClient(app)
     response1 = testclient.post("/child/add", json=dict(numberofclass=10, letterclass="B", age=16, name="Egor"))
@@ -14,5 +11,36 @@ def test_child():
     assert response.status_code == 200
     assert response.json() == "ok"
     
+
+def test_classroom():
+    testclient = TestClient(app)
+    response = testclient.post("/classroom/add", json=dict(number=585))
+    assert response.status_code == 200
+    assert response.json() == "ok"
+    response1 = testclient.post("/classroom/edit", json={"id" : 1, "number" : 321})
+    assert response1.status_code == 200
+    assert response1.json() == "ok"
+
+
+def test_teacher():
+    testclint = TestClient(app)
+    response = testclint.post("/teacher/add", json=dict(contract="sberb"))
+    assert response.status_code == 200
+    assert response.json() == "ok"
+    response1 = testclint.post("/teacher/edit", json={"id" : 1, "contract" : "t-bank"})
+    assert response1.status_code == 200
+    assert response1.json() == "ok"
+
+
+def test_lesson():
+    testclient = TestClient(app)
+    response = testclient.post("/lesson/add", json=dict(subject="rus", time="13:00:00"))
+    assert response.status_code == 200
+    assert response.json() == "ok"
+    response1 = testclient.post("/lesson/edit", json={"id" : 1, "subject" : "Math", "time" : "12:00:00"})
+    assert response1.status_code == 200
+    assert response1.json() == "ok"
+
+
 
 
